cddc617fab4f3bb914a7bf6ba146e5b9e151504e
Manthan Kuber <manthankuber873@gmail.com>
2022-03-13 18:08:40 +0530

    feat:Completed Home section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const nav = document.querySelector("nav");
   const burgerMenu = document.getElementById("burger-menu");
-  const navLinks = document.querySelectorAll(".nav-links");
+  const navLinks = document.querySelectorAll(".menu-container a");
 
   burgerMenu.addEventListener("click", () => {
     nav.classList.toggle("open");
